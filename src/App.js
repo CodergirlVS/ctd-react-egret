089492478a05ea895abafe9bfeb4e5720750077a
CodergirlVS <vaidehis1214@gmail.com>
2021-11-02 16:40:02 -0700

    prop added to the TodoContainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
 import TodoContainer from "./TodoContainer";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <TodoContainer />;
+  return (
+    <BrowserRouter>
+      {/* //<Route exact path="/"> */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/List1"> List1</Link>
+          </li>
+          <li>
+            <Link to="/List2">List2 </Link>
+          </li>
+        </ul>
+      </nav>
+      {/* //</Route> */}
+      <Switch>
+        <Route path="/List1">
+          <TodoContainer tableName={"List 1"} />
+        </Route>
+        <Route path="/List2">
+          <TodoContainer tableName={"List 2"} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 export default App;
--- src/TodoContainer.js
+++ src/TodoContainer.js
@@ -1,52 +1,35 @@
 import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
-function TodoContainer() {
-  const [todoList, setTodoList] = React.useState({
-    List1: [],
-    List2: [],
-  });
+function TodoContainer({ tableName }) {
+  const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [isError, setIsError] = React.useState(false);
 
-  const getFetch = () => {
-    const urlList1 = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/List%201?sort%5B0%5D%5Bfield%5D=Title&sort%5B0%5D%5Bdirection%5D=asc`;
-    const urlList2 = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID_LIST2}/List%202?sort%5B0%5D%5Bfield%5D=Title&sort%5B0%5D%5Bdirection%5D=asc`;
-
-    fetch(urlList1, {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-      },
-    })
-      .then((response) => response.json())
-      .then(
-        (result) => setTodoList({ List1: result.records }),
-        setIsLoading(false)
-      )
-      .catch(() => setIsError(true));
-
-    fetch(urlList2, {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-      },
-    })
-      .then((response) => response.json())
-      .then(
-        (result) => setTodoList({ List2: result.records }),
-        setIsLoading(false)
-      )
-      .catch(() => setIsError(true));
-  };
-
   React.useEffect(() => {
-    getFetch();
-  }, []);
+    fetch(
+      `https://api.airtable.com/v0/${
+        process.env.REACT_APP_AIRTABLE_BASE_ID
+      }/${encodeURI(
+        tableName
+      )}?sort%5B0%5D%5Bfield%5D=Title&sort%5B0%5D%5Bdirection%5D=asc`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => setTodoList(result.records), setIsLoading(false))
+      .catch(() => setIsError(true));
+  }, [tableName]);
 
   const addTodo = (newTodo) => {
     fetch(
-      `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`,
+      `https://api.airtable.com/v0/${
+        process.env.REACT_APP_AIRTABLE_BASE_ID
+      }/${encodeURI(tableName)}`,
       {
         method: "POST",
         headers: {
@@ -66,13 +49,15 @@ function TodoContainer() {
     )
       .then((response) => response.json())
       .then((data) => {
-        setTodoList([...todoList.List1, ...data.records]);
+        setTodoList([...todoList, ...data.records]);
       })
       .catch((error) => console.warn("error creating node", error));
   };
   const removeTodo = (id) => {
     fetch(
-      `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default?records[]=${id}`,
+      `https://api.airtable.com/v0/${
+        process.env.REACT_APP_AIRTABLE_BASE_ID
+      }/${encodeURI(tableName)}?records[]=${id}`,
       {
         method: "DELETE",
         headers: {
@@ -82,55 +67,23 @@ function TodoContainer() {
     )
       .then((response) => response.json())
       .then((data) =>
-        setTodoList(
-          todoList.List1.filter((item) => item.id !== data.records[0].id)
-        )
+        setTodoList(todoList.filter((item) => item.id !== data.records[0].id))
       );
   };
 
   return (
-    <BrowserRouter>
-      <Route path="/">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/List1"> List1</Link>
-            </li>
-            <li>
-              <Link to="/List2">List2 </Link>
-            </li>
-          </ul>
-        </nav>
-      </Route>
-      <Switch>
-        <Route path="/List1">
-          <>
-            <h1>Todo List</h1>
-            {isError && <p>Something went wrong ...</p>}
-            <AddTodoForm onAddTodo={addTodo} />
-            {isLoading ? (
-              <p>
-                <strong>Loading.....</strong>
-              </p>
-            ) : (
-              <TodoList todoList={todoList.List1} onRemoveTodo={removeTodo} />
-            )}
-          </>
-        </Route>
-        <Route path="/List2">
-          <h1>New Todo List</h1>
-          {isError && <p>Something went wrong ...</p>}
-          <AddTodoForm onAddTodo={addTodo} />
-          {isLoading ? (
-            <p>
-              <strong>Loading.....</strong>
-            </p>
-          ) : (
-            <TodoList todoList={todoList.List2} onRemoveTodo={removeTodo} />
-          )}
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <h1>{tableName}</h1>
+      {isError && <p>Something went wrong ...</p>}
+      <AddTodoForm onAddTodo={addTodo} />
+      {isLoading ? (
+        <p>
+          <strong>Loading.....</strong>
+        </p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
+    </>
   );
 }
 
